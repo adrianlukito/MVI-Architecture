d15e79da062edc5d0f993ff3865d6c132d697739
adrianlukito <lukito.adrian1@gmail.com>
2021-08-04 21:54:36 +0700

    StateEvents

--- app/src/main/java/com/codingwithmitch/mviexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/codingwithmitch/mviexample/ui/main/MainViewModel.kt
@@ -2,12 +2,35 @@ package com.codingwithmitch.mviexample.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.codingwithmitch.mviexample.ui.main.state.MainStateEvent
 import com.codingwithmitch.mviexample.ui.main.state.MainViewState
+import com.codingwithmitch.mviexample.util.AbsentLiveData
 
 class MainViewModel: ViewModel() {
 
+    private val _stateEvent: MutableLiveData<MainStateEvent> = MutableLiveData()
     private val _viewState: MutableLiveData<MainViewState> = MutableLiveData()
+
     val viewState: LiveData<MainViewState>
         get() = _viewState
+
+    val dataState: LiveData<MainViewState> = Transformations.switchMap(_stateEvent) { stateEvent ->
+        handleStateEvent(stateEvent)
+    }
+
+    private fun handleStateEvent(stateEvent: MainStateEvent?): LiveData<MainViewState> {
+        when(stateEvent) {
+            is MainStateEvent.GetBlogPostsEvent -> {
+                return AbsentLiveData.create()
+            }
+            is MainStateEvent.GetUserEvent -> {
+                return AbsentLiveData.create()
+            }
+            is MainStateEvent.None -> {
+                return AbsentLiveData.create()
+            }
+        }
+    }
 }
